d442a42bb30741edd7c753589b77a26537be108b
yubeenso <youbin0105@gmail.com>
2023-03-31 06:13:50 +0900

    섹션 3 add file

---
+++ 섹션 3 - JSX/3-3. (실습) JSX 코드 작성해보기/chapter_03/index.js
@@ -0,0 +1,20 @@
+// 우리가 만든 컴포넌트를 실제로 화면에 랜더링하기 위한 js 파일
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import APP from './App';
+import reportWebVitals from './reportWebVitals';
+
+import Library from './chapter_03/Library';     // import를 사용해 방금 만든 Library 컴포넌트를 가져옴. 
+
+ReactDOM.render(     // ReactDOM을 사용하여 root DOM node에 랜더링하도록 하는 코드
+  <React.StrictMode>
+    <Library />
+  </React.StrictMode>,     // 파라미터를 구분하는 쉼표(,)가 있어야 오류가 나지 않음!
+  document.getElementById('root')     
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
